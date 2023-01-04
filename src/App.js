39d86734d8d4d5734b275bdb09b8fba1b3e6e517
Aneta Borowski <borowski.aneta@gmail.com>
2023-01-04 16:22:15 +0100

    toggleHideDone interaction

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import Form from "./Form";
 import List from "./List";
-import Buttons  from "./Buttons";
+import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import { useState } from "react";
 
 const tasks = [
-  { id: 1, content: "Przejść na Reacta", done: false},
-  { id: 2, content: "Zjeść obiad", done: true},
+  { id: 1, content: "Przejść na Reacta", done: false },
+  { id: 2, content: "Zjeść obiad", done: true },
 ];
 
-const hideDone = false;
-
 function App() {
+
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
-      <Section 
-      title="Dodaj nowe zadanie" 
-      body={<Form />} 
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
       />
       <Section
-      title="Lista zadań"
-      body={<List tasks={tasks} hideDone={hideDone} />}
-      extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} />}
+        title="Lista zadań"
+        body={<List tasks={tasks} hideDone={hideDone} />}
+        extraHeaderContent={<Buttons
+          tasks={tasks}
+          hideDone={hideDone}
+          toggleHideDone={toggleHideDone} />}
       />
     </Container>
   );
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,19 +1,19 @@
 import "./style.css";
 
-const Buttons = ({tasks, hideDone}) => (
-tasks.length > 0 && (
-    <div className="tasks">
-            <button className="tasks__buttons">
-    {hideDone ? "Pokaż" : "Ukryj"} ukończone
-    </button>
-    <button className="tasks__buttons"
-    disabled={tasks.every(({ done }) => done)}
-   >
-    Ukończ wszystkie
-    </button>
-        </div>
-))
-   
+const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
+        tasks.length > 0 && (
+                <div className="tasks">
+                        <button onClick={toggleHideDone} className="tasks__buttons">
+                                {hideDone ? "Pokaż" : "Ukryj"} ukończone
+                        </button>
+                        <button className="tasks__buttons"
+                                disabled={tasks.every(({ done }) => done)}
+                        >
+                                Ukończ wszystkie
+                        </button>
+                </div>
+        ))
+
 
 
 export default Buttons;
