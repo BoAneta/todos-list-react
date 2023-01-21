8562c7ba9c9f6df2242bb8c91cff0ce4edcae36d
Aneta Borowski <borowski.aneta@gmail.com>
2023-01-21 20:54:05 +0100

    if else changed to conditional operator

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ import { useState } from "react";
 function App() {
 
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState(
-    []
-  );
+  const [tasks, setTasks] = useState([]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
@@ -22,12 +20,9 @@ function App() {
   };
 
   const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-        return { ...task, done: !task.done };
-      }
-      return task;
-    }));
+    setTasks(tasks => tasks.map(
+      task => task.id === id ? { ...task, done: !task.done } : task
+      ));
   };
 
   const setAllDone = () => {
@@ -43,7 +38,7 @@ function App() {
       {
         content: newTaskContent,
         done: false,
-        id: tasks.length ? tasks[tasks.length -1].id + 1 : 1,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
       }
     ])
   };
