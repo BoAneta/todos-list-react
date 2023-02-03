cb43581bc2978af8bf64a37a58152d216ec8e833
Aneta Borowski <borowski.aneta@gmail.com>
2023-02-03 18:58:37 +0100

    Buttons index.js formated

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,21 +1,21 @@
 import { Tasks, TasksButtons } from "./styled";
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-        tasks.length > 0 && (
-                <Tasks>
-                        <TasksButtons 
-                                onClick={toggleHideDone}
-                        >
-                                {hideDone ? "Pokaż" : "Ukryj"} ukończone
-                        </TasksButtons>
-                        <TasksButtons
-                                onClick={setAllDone}
-                                disabled={tasks.every(({ done }) => done)}
-                        >
-                                Ukończ wszystkie
-                        </TasksButtons>
-                </Tasks>
-        ))
+    tasks.length > 0 && (
+        <Tasks>
+            <TasksButtons
+                onClick={toggleHideDone}
+            >
+                {hideDone ? "Pokaż" : "Ukryj"} ukończone
+            </TasksButtons>
+            <TasksButtons
+                onClick={setAllDone}
+                disabled={tasks.every(({ done }) => done)}
+            >
+                Ukończ wszystkie
+            </TasksButtons>
+        </Tasks>
+    ))
 
 
 
