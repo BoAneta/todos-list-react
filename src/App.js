45444190ada39b4f61e202c07cc89519370fd2df
Aneta Borowski <borowski.aneta@gmail.com>
2023-03-06 12:29:41 +0100

    Author page added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
+import { HashRouter, Link, Switch, Route } from "react-router-dom";
+import AuthorPage from "./features/tasks/AuthorPage";
 import Tasks from "./features/tasks/Tasks";
 
-export const App = () => <Tasks />;
+export const App = () => (
+    <HashRouter>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/zadania">Zadania</Link>
+                </li>
+                <li>
+                    <Link to="/autor">O autorze</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route path="/zadania">
+                <Tasks />
+                </Route>
+                <Route path="/autor">
+                <AuthorPage />
+                </Route>
+            </Switch>
+        </nav>
+    </HashRouter>
+);
---
+++ src/features/tasks/AuthorPage/index.js
@@ -0,0 +1,24 @@
+import Container from "../../../common/Container";
+import Section from "../../../common/Section";
+
+export default () => (
+  <Container>
+    <h1>O autorze</h1>
+    <Section
+      title="Aneta Borowski"
+      body={
+        <>
+          <p>
+            Nazywam się <b>Aneta Borowski</b>. Pochodzę z Wrocławia, ale obecnie
+            mieszkam w <em>Sandnes w Norwegii</em>. Z zawodu jestem specjaliską
+            ds. Public Relations, jednak od lat nie pracowałam w tym fachu.
+            Większą część życia spędziłam podróżując i pracując za granicą.
+            Oprócz podróży uwielbiam czekoladę, zwierzęta (mam psiaka adapciaka
+            Foresta) oraz ludzi. Kilka lat temu trenowałam Lacrosse, a przez
+            ostatnie dwa lata swojej kariery byłam trenerką.
+          </p>
+        </>
+      }
+    />
+  </Container>
+);
